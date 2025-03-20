ecee9fcd20d92a0c6a5afe951c35471e48f43954
Bung1e <leshkachovzhik@gmail.com>
2025-03-20 16:39:35 +0100

    bug fixes

--- api/api.py
+++ api/api.py
@@ -12,4 +12,4 @@ async def predict():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
